70aa7df8a0310021813967a0a8a2aa76b25e2deb
jon1267 <jonleev@gmail.com>
2018-08-03 16:39:43 +0300

    Melkie pravki

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
-
 Route::get('/', 'IndexController@blogHome')->name('index.blogHome');
 Route::get('/blog/{id}', 'IndexController@blogSingle')->name('index.blogSingle');
+
 //Store comment to article. front page from all users (guests)
 Route::resource('comment', 'CommentController', ['only'=>['store']]);
 
